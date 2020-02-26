2d08422b74fe21c5f4effcaef8f629ccddab1d75
sanchitd5 <sanchitd5@live.com>
2020-02-26 11:52:18 +1100

    Remove adaptiveWidgets

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,23 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../providers/providers.dart';
-import '../widgets/helpers/AdaptiveScaffold.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AdaptiveScaffold(
-        appBarTitle: 'Flutter User OnBoarding',
-        appBarActions: GestureDetector(
-            onTap: () {},
-            child: Consumer<UserDataProvider>(
-                builder: (_, data, __) => FlatButton(
-                      child: Text('Logout'),
-                      onPressed: () {
-                        data.logout();
-                        Navigator.pushReplacementNamed(context, '/');
-                      },
-                    ))),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter User OnBoarding'),
+          actions: <Widget>[
+            GestureDetector(
+                onTap: () {},
+                child: Consumer<UserDataProvider>(
+                    builder: (_, data, __) => FlatButton(
+                          child: Text('Logout'),
+                          onPressed: () {
+                            data.logout();
+                            Navigator.pushReplacementNamed(context, '/');
+                          },
+                        )))
+          ],
+        ),
         body: Container(
           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
           child: Consumer<UserDataProvider>(
