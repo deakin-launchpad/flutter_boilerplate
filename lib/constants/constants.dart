ff30fa32d09e282ad23984cd96f5fb39cbffd7db
Qiaoliwang2020 <414217795zoe@gmail.com>
2022-07-22 09:12:52 +1000

    upgrated flutter to 3.1.0

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,14 +1,15 @@
 import 'application.dart';
 import 'connection.dart';
-import 'amplifyconfiguration.dart';
+// import 'amplifyconfiguration.dart';
 
 import '../models/models.dart';
 
 class Constants {
   static ApplicationConstants applicationConstants = ApplicationConstants();
   static ConnectionConstants connectionConstants = ConnectionConstants();
-  static const bool amplifyEnabled = true;
-  static const String? _amplifyConfig = amplifyEnabled ? amplifyconfig : null;
+  static const bool amplifyEnabled = false;
+  // static const String? _amplifyConfig = amplifyEnabled ? amplifyconfig : null;
+  static const String? _amplifyConfig = null;
   static const String OTP_GIF_IMAGE = "lib/assets/img/logo/otp.gif";
   static const bool devBuild = true;
   static const bool debugBanner = false;
